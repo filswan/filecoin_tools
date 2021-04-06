6f46b8febd0cd950f367807e6f714081bf7a4874
Charles Cao <caotaotao@gmail.com>
2021-04-05 21:49:58 -0400

    Update sector_updater.py

--- sector_updater.py
+++ sector_updater.py
@@ -4,32 +4,32 @@ import time
 
 EPOCHE_PER_DAY = 24 * 3600 / 30
 
-location = '/var/logs/sectors'
-
+filename = '/var/logs/sectors'
 
 def get_current_epoch_by_current_time():
     current_timestamp = int(time.time())
     current_epoch = int((current_timestamp - 1598306471) / 30)
     return current_epoch
 
-for filename in os.listdir(location):
-    if filename == 'sectors.log':
-        print(filename)
-        file = open(os.path.join(location, filename), "r")
-        lines = file.read().splitlines()
-        file.close()
-        current_epoch = get_current_epoch_by_current_time()
 
-        for line in lines:
-            sector_pattern = re.compile(r"([0-9]+) .* ")
-            sector_number = sector_pattern.findall(line)
+print(filename)
+file = open(filename, "r")
+lines = file.read().splitlines()
+file.close()
+current_epoch = get_current_epoch_by_current_time()
+new_epoch = current_epoch + EPOCHE_PER_DAY + 540
+for line in lines:
+    sector_pattern = re.compile(r"([0-9]+) .* ")
+    sector_number = sector_pattern.findall(line)
 
-            expiration_pattern = re.compile(r"\D(\d{3,})\D")
-            expiration = expiration_pattern.findall(line)
+    expiration_pattern = re.compile(r"\D(\d{3,})\D")
+    expiration = expiration_pattern.findall(line)
 
-            if expiration and expiration:
-                remaining_days = (int(expiration[0]) - current_epoch) / (EPOCHE_PER_DAY)
-                if remaining_days < 30:
-                    expiration_weeks = remaining_days / 7
-                    expiration_days = remaining_days % 7
-                    print(line)
+    if expiration and expiration:
+        remaining_days = (int(expiration[0]) - current_epoch) / (EPOCHE_PER_DAY)
+        if remaining_days < 2:
+            expiration_weeks = remaining_days / 7
+            expiration_days = remaining_days % 7
+            print(line)
+            print("Command for extend 540 days: ")
+            print('lotus-miner sectors extend --new-expiration %d 2870 %d' % (new_epoch, int(sector_number[0])))
